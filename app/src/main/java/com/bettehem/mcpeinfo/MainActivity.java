15b12dcc6b64c6911529fca37508b92f5120dda4
Bettehem <chris.mustola@gmail.com>
2015-02-22 15:26:21 +0200

    Added license notices to files

--- app/src/main/java/com/bettehem/mcpeinfo/MainActivity.java
+++ app/src/main/java/com/bettehem/mcpeinfo/MainActivity.java
@@ -1,5 +1,22 @@
 package com.bettehem.mcpeinfo;
 
+/*
+    Copyright 2015  Chris Mustola
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -28,6 +45,7 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
     }
+
 
 
     @Override
