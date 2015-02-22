48115300882dc18118b63f9cd89e442a10b1d9ec
Bettehem <chris.mustola@gmail.com>
2015-02-22 14:54:16 +0200

    Added a toolbar

--- app/src/main/java/com/bettehem/mcpeinfo/MainActivity.java
+++ app/src/main/java/com/bettehem/mcpeinfo/MainActivity.java
@@ -2,16 +2,31 @@ package com.bettehem.mcpeinfo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        variables();
+    }
+
+
+    public void variables(){
+        toolbars();
+    }
+
+    public void toolbars(){
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
